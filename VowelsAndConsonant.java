d3443066cd2b1e8270c1753f384528fc27290dcc
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-09-24 16:58:48 +0530

    added:Programs

---
+++ VowelsAndConsonant.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.day5;
+import java.util.Scanner;
+public class VowelsAndConsonant {
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("enter the alphabet");
+        char input = sc.next().charAt(0);
+        if(input == 'a' || input == 'e' || input == 'i' || input == 'o'|| input == 'u')
+            System.out.println(input+ " is Vowel");
+        else
+            System.out.println(input+" is Consonant");
+    }
+
+}
+
